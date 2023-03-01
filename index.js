1fc2237058c3c8cc8a38302ad738aff8f59a4727
mas-fahmi <donkult23@gmail.com>
2023-03-01 20:08:54 +0700

    response

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const bodyParser = require("body-parser");
+const con = require("./connection");
+const response = require("./response")
 
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    response(200, "api ready to use", "ready", res)
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
